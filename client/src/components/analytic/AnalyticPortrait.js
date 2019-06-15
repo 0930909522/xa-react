5726d451c7021cc8b382a5cec81158446a945b5e
濮世禹 <buzz.pu@gmail.com>
2019-06-15 16:32:14 +0800

    魚群圖, 動態圓圈

--- client/src/components/analytic/AnalyticPortrait.js
+++ client/src/components/analytic/AnalyticPortrait.js
@@ -5,7 +5,23 @@ import Footer from '../Footer';
 import { Container, Row, Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
 
 class AnalyticPortrait extends Component {
-    state = {}
+    constructor(props) {
+        super(props);
+        this.state = {
+            fish: [
+                { "id": "g1", "name": "海豚" },
+                { "id": "g2", "name": "烏龜" },
+                { "id": "g3", "name": "蝦子" },
+                { "id": "g4", "name": "鮟鱇魚" },
+                { "id": "g5", "name": "鮪魚" },
+                { "id": "g6", "name": "鯊魚" },
+                { "id": "g7", "name": "鯨魚" }
+            ],
+            newData: "",
+            basic: "",
+        }
+    }
+
     render() {
         return (<>
             <Header />
@@ -15,7 +31,12 @@ class AnalyticPortrait extends Component {
                         <NavLeft />
                         <div className="main_right">
                             <h2>用戶畫像</h2>
-                            <div className="box"></div>
+                            <div className="box">
+                                <div className="icon_box">
+                                    <div className={this.state.fish[3].id + " icon"}  ></div>
+                                    {this.state.fish[3].name}
+                                </div>
+                            </div>
                         </div>
                     </Row>
                 </Container>
