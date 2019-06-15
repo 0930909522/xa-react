6ae936e13cefd4531b734a01b2ee163b6c934278
濮世禹 <buzz.pu@gmail.com>
2019-06-15 16:47:51 +0800

    魚群製作

--- client/src/components/analytic/AnalyticPortrait.js
+++ client/src/components/analytic/AnalyticPortrait.js
@@ -3,11 +3,14 @@ import NavLeft from './NavLeft';
 import Header from '../Header';
 import Footer from '../Footer';
 import { Container, Row, Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 class AnalyticPortrait extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            fishNow: 6,
             fish: [
                 { "id": "g1", "name": "海豚" },
                 { "id": "g2", "name": "烏龜" },
@@ -23,6 +26,7 @@ class AnalyticPortrait extends Component {
     }
 
     render() {
+        const percentage = 66;
         return (<>
             <Header />
             <div className="layout_main">
@@ -33,9 +37,23 @@ class AnalyticPortrait extends Component {
                             <h2>用戶畫像</h2>
                             <div className="box">
                                 <div className="icon_box">
-                                    <div className={this.state.fish[3].id + " icon"}  ></div>
-                                    {this.state.fish[3].name}
+                                    <div className={this.state.fish[this.state.fishNow].id + " icon"}  ></div>
+                                    {this.state.fish[this.state.fishNow].name}
                                 </div>
+                                <div className="progress_round">
+                                    <ul>
+                                        <li>
+                                            <CircularProgressbar value={percentage} text={`${percentage}%`} />
+                                        </li>
+                                        <li>
+                                            <CircularProgressbar value={percentage} text={`${percentage}%`} />
+                                        </li>
+                                        <li>
+                                            <CircularProgressbar value={percentage} text={`${percentage}%`} />
+                                        </li>
+                                    </ul>
+                                </div>
+
                             </div>
                         </div>
                     </Row>
